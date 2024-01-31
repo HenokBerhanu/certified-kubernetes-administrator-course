48fe6dfeffd427563b23339b605e8fd7b33165b5
henok berhanu tsegaye <henokberhanut@gmail.com>
2024-01-31 14:06:10 +0100

    increase number of node

--- kubeadm-clusters/virtualbox/ubuntu/vagrant/setup-hosts.sh
+++ kubeadm-clusters/virtualbox/ubuntu/vagrant/setup-hosts.sh
@@ -16,6 +16,8 @@ sed -e "/^.*$2.*/d" -i /etc/hosts
 cat >> /etc/hosts <<EOF
 ${NETWORK}.11  kubemaster
 ${NETWORK}.21  kubenode01
+${NETWORK}.22  kubenode02
+${NETWORK}.23  kubenode03
 EOF
 
 # Expoert internal IP as an environment variable
