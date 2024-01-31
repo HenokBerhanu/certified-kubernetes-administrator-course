ce7a33d99b2e78b02b80c514c5f4e4a5666de30c
henok berhanu tsegaye <henokberhanut@gmail.com>
2024-01-31 18:19:46 +0100

    update

--- kubeadm-clusters/virtualbox/ubuntu/vagrant/controlplane.sh
+++ kubeadm-clusters/virtualbox/ubuntu/vagrant/controlplane.sh
@@ -1,10 +1,60 @@
-{
-POD_CIDR=10.244.0.0/16
-SERVICE_CIDR=10.96.0.0/16
+# {
+# POD_CIDR=10.244.0.0/16
+# SERVICE_CIDR=10.96.0.0/16
 
-kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $INTERNAL_IP
+# kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $INTERNAL_IP
 
-kubectl --kubeconfig /etc/kubernetes/admin.conf \
-    apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
+# kubectl --kubeconfig /etc/kubernetes/admin.conf \
+#     apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
 
-}
+# }
+
+# I use only this command on the master node
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.56.11
+
+# The output of the following command looks like,
+
+Your Kubernetes control-plane has initialized successfully!
+
+To start using your cluster, you need to run the following as a regular user:
+
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+Alternatively, if you are the root user, you can run:
+
+  export KUBECONFIG=/etc/kubernetes/admin.conf
+
+You should now deploy a pod network to the cluster.
+Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
+  https://kubernetes.io/docs/concepts/cluster-administration/addons/
+
+Then you can join any number of worker nodes by running the following on each as root:
+
+kubeadm join 192.168.56.11:6443 --token 3p1v60.sw7xnadhibqd4wp3 \
+	--discovery-token-ca-cert-hash sha256:5888f82686ee57b643fcf4e7cb6929681fba5f770efe6a749d0e2e7012ca8100
+
+# Follow the above instruction (coppying the above three kube config commands on the master node) and after that before installing CNI, do this on the master node
+
+# run this command to verify that the permission is 644 or above
+stat -c %a /etc/kubernetes/admin.conf
+# If not 644, run the followinf command,
+chmod 644 /etc/kubernetes/admin.conf
+# After that do this to swapoff 
+
+sudo -i
+swapoff -a
+exit 
+strace -eopenat kubectl version
+
+# Now you can install CNI
+
+# I choose calico for my cluster
+
+# Install the Tigera Calico operator and custom resource definitions
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml >/dev/null
+# Install Calico by creating the necessary custom resource
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml >/dev/null
+# Confirm that all of the pods are running with the following command
+watch kubectl get pods -n calico-system
--- kubeadm-clusters/virtualbox/ubuntu/vagrant/node-setup.sh
+++ kubeadm-clusters/virtualbox/ubuntu/vagrant/node-setup.sh
@@ -1,48 +1,78 @@
-{
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
+# this installation is for k8s v1.29 on ubuntu jammy
+# Go to k8s documentation and fearch for installing kubeadm
 
-cat <<EOF > /etc/modules-load.d/k8s.conf
+
+# Install container runtime (CRI) first....CRI-O, containerd...etc. I choose containerd
+# Install and configure prerequisites 
+
+# Step-1: Forwarding IPv4 and letting iptables see bridged traffic
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
-
-modprobe overlay
-modprobe br_netfilter
-
-cat <<EOF > /etc/sysctl.d/k8s.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
+# Apply sysctl params without reboot
+sudo sysctl --system
 
-sysctl --system
+# Step-2: Verify that the br_netfilter, overlay modules are loaded by running the following commands
 
-apt-get install -y containerd
+lsmod | grep br_netfilter
+lsmod | grep overlay
 
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+# Step-3: Verify that the net.bridge.bridge-nf-call-iptables, net.bridge.bridge-nf-call-ip6tables, and net.ipv4.ip_forward system variables are set to 1
 
-systemctl restart containerd
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
 
-KUBE_LATEST=$(curl -L -s https://dl.k8s.io/release/stable.txt | awk 'BEGIN { FS="." } { printf "%s.%s", $1, $2 }')
+# Step-4: Install containerd
 
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# A).  Set up Docker's apt repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBE_LATEST}/deb/ /" > /etc/apt/sources.list.d/kubernetes.list
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+# B). Install the latest version of containerd
 
-crictl config \
-    --set runtime-endpoint=unix:///run/containerd/containerd.sock \
-    --set image-endpoint=unix:///run/containerd/containerd.sock
+sudo apt-get install containerd.io
 
-cat <<EOF > /etc/default/kubelet
-KUBELET_EXTRA_ARGS='--node-ip $(ip -4 addr show enp0s8 | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)'
-EOF
-}
+# Step-5: If you experience container crash loops after the initial cluster installation or after installing a CNI do this (create default configuration)
+
+#sudo mkdir -p /etc/containerd
+sudo containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' | sudo tee /etc/containerd/config.toml
+# and restart containerd
+sudo systemctl restart containerd
+
+# Step-6: These instructions are for Kubernetes 1.29.
+   # A). Update the apt package index and install packages needed to use the Kubernetes apt repository:
+      sudo apt-get update
+      # apt-transport-https may be a dummy package; if so, you can skip that package
+      sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+   # B). Download the public signing key for the Kubernetes package repositories
+      # If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+      # sudo mkdir -p -m 755 /etc/apt/keyrings
+      sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+   # C). Add the appropriate Kubernetes apt repository. Please note that this repository have packages only for Kubernetes 1.29.
+      # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+      sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   # D). Update the apt package index, install kubelet, kubeadm and kubectl, and pin their version
+      sudo apt-get update
+      sudo apt-get install -y kubelet kubeadm kubectl
+      sudo apt-mark hold kubelet kubeadm kubectl
 
